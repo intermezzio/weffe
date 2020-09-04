16cd433df2278167e7befa7cf91653f537c71b27
Andrew Mascillaro <amascillaro@gmail.com>
2020-09-04 13:16:30 -0400

    Fix functionality for meme strings with webcam input

--- video_looper_complete.sh
+++ video_looper_complete.sh
@@ -65,10 +65,10 @@ then
 	
 	memestr="drawtext=font='$font': \
 		text='$toptext': x=(w-tw)/2: y=(h-text_h)/8: \
-		fontsize=$toptextfs: borderw=4: fontcolor=AntiqueWhite, \
-		drawtext=font='$font':\
+		fontsize=$toptextfs: fontcolor='AntiqueWhite': borderw=4, \
+		drawtext=font='$font': \
 		text='$bottomtext':x=(w-tw)/2:y=7*(h-text_h)/8: \
-		fontsize=$bottomtextfs: borderw=4: fontcolor=AntiqueWhite,"
+		fontsize=$bottomtextfs: fontcolor='AntiqueWhite': borderw=4, "
 fi
 
 if [[ $rotate = true ]]
@@ -98,20 +98,24 @@ then # if streaming a video
 
 	if [[ $rotate = true ]]
 	then # rotate video and stream it
-		ffmpeg -stream_loop -1 -re -i "$video" -vf "$memestr $rotatestr format=yuv420p[v]" -map 0:v -f v4l2 "/dev/video$output"
+		ffmpeg -stream_loop -1 -re -i "$video" -vf "$memestr $rotatestr format=yuv420p[v]" \
+			-map 0:v -f v4l2 "/dev/video$output"
 	else
-		ffmpeg -stream_loop -1 -re -i "$video" -vf "$memestr $rotatestr format=yuv420p[v]" -map 0:v -f v4l2 "/dev/video$output"
+		ffmpeg -stream_loop -1 -re -i "$video" -vf "$memestr $rotatestr format=yuv420p[v]" \
+			-map 0:v -f v4l2 "/dev/video$output"
 	fi
 else # if streaming from a webcam
 	if [[ $watermark != false ]]
 	then
 		ffmpeg -i "/dev/video$input" -i "$watermark" \
-			-filter_complex "[1][0]scale2ref[i][m];[m][i]overlay=format=auto,$memestr $rotatestr format=yuv420p[v]" \
+			-filter_complex "[1][0]scale2ref[i][m];[m][i]overlay=format=auto, \
+			$memestr $rotatestr format=yuv420p[v]" \
 			-map "[v]" -f v4l2 "/dev/video$output"
 	else
 		ffmpeg -i "/dev/video$input" \
-			-filter_complex "$memestr $rotatestr format=yuv420p[v]" \
-			-map "[v]" -f v4l2 "/dev/video$output"
+			-vf "$memestr $rotatestr format=yuv420p[v]" \
+			-map 0:v -f v4l2 "/dev/video$output"
+
 	fi
 fi
 # ffmpeg -i $norm -c copy v1.mp4
@@ -129,3 +133,5 @@ fi
 # ffmpeg -stream_loop -1 -re -i "$video.mp4" -map 0:v -f v4l2 /dev/video2
 
 # sh rotate_video.sh "$now.mp4"
+
+# ffmpeg -f v4l2 -input_format yuyv422 -video_size 800x448 -framerate 30 -i /dev/video0 -filter_complex "[0]vflip,drawtext=fontfile=/usr/share/fonts/truetype/freefont/Roboto-Regular.ttf:text='%{localtime\:%d %b %Y}':x=8:y=8:fontcolor=white:box=1:boxcolor=black@0.75,drawtext=fontfile=/usr/share/fonts/truetype/freefont/Roboto-Regular.ttf:text='%{localtime\:%T}':x=8:y=24:fontcolor=white:box=1:boxcolor=black@0.75[vid];[0]fps=1/20,vflip[img]" -map "[vid]" -c:v h264_omx -b:v 2M -f rtsp rtsp://localhost:8554/mystream -map "[img]" -update 1 /dev/shm/snapshot.jpg
