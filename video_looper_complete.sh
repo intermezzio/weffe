c521eb7f1ad5519eb493b270b95987422021cdd9
Andrew Mascillaro <amascillaro@gmail.com>
2021-11-26 00:32:21 -0500

    Add print ffmpeg statement option

--- video_looper_complete.sh
+++ video_looper_complete.sh
@@ -25,9 +25,10 @@ blurtype=""
 blurstr=""
 nextinput=1
 inputstr=""
+filterstr=""
+printffmpeg=false
 
-while getopts ":hv:rsi:o:w:t:b:f:z:B:" opt; do
-  # echo $opt
+while getopts ":hv:rsi:o:w:t:b:f:z:B:p" opt; do
   case $opt in
     v) video="$OPTARG"
     ;;
@@ -40,7 +41,6 @@ while getopts ":hv:rsi:o:w:t:b:f:z:B:" opt; do
 	o) output="$OPTARG"
 	;;
 	w) watermark="$OPTARG"
-		printf "watermark: $watermark"
     ;;
     t) toptext="$OPTARG"
 	;;
@@ -56,11 +56,28 @@ while getopts ":hv:rsi:o:w:t:b:f:z:B:" opt; do
     B) blur=true
 		blurtype=$OPTARG
 	;;
+	p) printffmpeg=true
+	;;
     \?) echo "Invalid option -$OPTARG" >&2
 		exit 0
     ;;
   esac
 done
+
+if [[ $video != false ]]
+then # if streaming a video
+	if [[ $process = true ]]
+	then # process the video input
+        ffmpeg -y -i "$video" -filter_complex "[0]split=2[fr][rv]; \
+            [rv]reverse[rv];[fr][rv]concat=n=2:v=1:a=0,format=yuv420p[v]" \
+            -map "[v]" -g 30 stream.mp4
+    else
+        \cp -r "$video" stream.mp4
+    fi
+    inputstr="-stream_loop -1 -re -i stream.mp4"
+else # if streaming from a webcam
+	inputstr="-i /dev/video$input"
+fi
 
 if [ $blur = true ]
 then
@@ -121,128 +138,76 @@ then
 			exit 0
 		;;
 	esac
+	filterstr="${filterstr}${blurstr}"
+fi
+
+if [[ $watermark != false ]]
+then
+	waterstr="[$nextinput][v]scale2ref[i][m];[m][i]overlay=format=auto[v];"
+	nextinput=$((nextinput+1))
+	inputstr="${inputstr} -i $watermark"
+	filterstr="${filterstr}${waterstr}"
 fi
 
 if [[ -f $toptext ]] && [[ -f $bottomtext ]]
 then
 	topplaintext=$(more $toptext | tr '[:lower:]' '[:upper:]')
-	printf "%s\n" "$topplaintext"
 	toptextlen=$(expr length "$toptext")
-	printf "%s\n" "$toptextlen"
 	toptextfs=$((240 / ($toptextlen / 5 + 1) ))
-	printf "%s\n" $toptextfs
-
+	
 	bottomplaintext=$(more $bottomtext | tr '[:lower:]' '[:upper:]')
 	bottomtextlen=$(expr length "$bottomplaintext")
-	printf "%s\n" "$bottomplaintextlen"
 	bottomtextfs=$((240 / ($bottomtextlen / 5 + 1) ))
-	printf "%s\n" $bottomtextfs
-
+	
 	memestr="[v]drawtext=font='$font': \
 		textfile='$toptext': x=(w-tw)/2: y=(h-text_h)/8: reload=1: \
 		fontsize=$toptextfs: fontcolor='AntiqueWhite': borderw=4, \
 		drawtext=font='$font': \
 		textfile='$bottomtext':x=(w-tw)/2:y=7*(h-text_h)/8: reload=1: \
 		fontsize=$bottomtextfs: fontcolor='AntiqueWhite': borderw=4[v];"
+
+	filterstr="${filterstr}${memestr}"
 elif [[ $toptext != false ]] && [[ $bottomtext != false ]]
 then
 	toptext=$(echo $toptext | tr '[:lower:]' '[:upper:]')
-	printf "%s\n" "$toptext"
 	toptextlen=$(expr length "$toptext")
-	printf "%s\n" "$toptextlen"
 	toptextfs=$((240 / ($toptextlen / 5 + 1) ))
-	printf "%s\n" $toptextfs
-
+	
 	bottomtext=$(echo $bottomtext | tr '[:lower:]' '[:upper:]')
 	bottomtextlen=$(expr length "$bottomtext")
-	printf "%s\n" "$bottomtextlen"
 	bottomtextfs=$((240 / ($bottomtextlen / 5 + 1) ))
-	printf "%s\n" $bottomtextfs
-
+	
 	memestr="[v]drawtext=font='$font': \
 		text='$toptext': x=(w-tw)/2: y=(h-text_h)/8: \
 		fontsize=$toptextfs: fontcolor='AntiqueWhite': borderw=4, \
 		drawtext=font='$font': \
 		text='$bottomtext':x=(w-tw)/2:y=7*(h-text_h)/8: \
 		fontsize=$bottomtextfs: fontcolor='AntiqueWhite': borderw=4[v];"
+
+	filterstr="${filterstr}${memestr}"
 fi
 
 if [[ $rotate != false ]]
 then
 	rotatestr="[v]rotate=2*PI*t/6[v];"
+	filterstr="${filterstr}${rotatestr}"
 fi
 
-if [[ $video != false ]]
-then # if streaming a video
-	if [[ $process = true ]]
-	then # process the video input
-        ffmpeg -y -i "$video" -filter_complex "[0]split=2[fr][rv]; \
-            [rv]reverse[rv];[fr][rv]concat=n=2:v=1:a=0,format=yuv420p[v]" \
-            -map "[v]" -g 30 stream.mp4
-    else
-        \cp -r "$video" stream.mp4
-    fi
+# remove whitespace from filter string
+filterstr=$(echo "$filterstr" | sed -r 's/\s+//g')
 
-		# norm="$video"
-		# rev="rev-$video"
-		# now="$(date)"
-    #
-		# ffmpeg -i $norm -vf reverse $rev
-    #
-		# printf "file '%s'\nfile '%s'" "$norm" "$rev" > "$now.txt"
-    #
-		# ffmpeg -f concat -i "$now.txt" -c copy "$now.mp4"
-    #
-		# rm $rev "$now.txt"
-    #
-		# video="$now.mp4"
+filterstr="[0]split=1[v];${filterstr}[v]format=yuv420p[v]"
+ffmpegstr="ffmpeg $inputstr \
+	-filter_complex \"${filterstr}\" \
+	-map "[v]" -f v4l2 \"/dev/video$output\""
 
-		# stack overflow
-		# ffmpeg -i input.mkv -filter_complex "[v]reverse,split=3[r1][r2][r3];[v][r1][v][r2][v][r3] concat=n=6:v=1[v]" -map "[v]" output.mkv
+# remove more whitespace
+ffmpegstr=$(echo "$ffmpegstr" | sed -r 's/\s+/ /g')
 
-	if [[ $watermark != false ]]
-	then # rotate video and stream it
-		# ffmpeg -stream_loop -1 -re -i "$video" -vf "$memestr $rotatestr format=yuv420p[v]" \
-			# -map 0:v -f v4l2 "/dev/video$output"
-		ffmpeg -stream_loop -1 -re -i stream.mp4 -i "$watermark" \
-			-filter_complex "$blurstr [1][0]scale2ref[i][m];[m][i]overlay=format=auto[v]; \
-			$memestr [v]$rotatestr format=yuv420p[v]" \
-			-map "[v]" -f v4l2 "/dev/video$output"
-	else
-		ffmpeg -stream_loop -1 -re -i stream.mp4 $inputstr \
-			-filter_complex "$blurstr $memestr $rotatestr [v]format=yuv420p[v]" \
-			-map "[v]" -f v4l2 "/dev/video$output"
-	fi
-else # if streaming from a webcam
-	if [[ $watermark != false ]]
-	then
-		ffmpeg -i "/dev/video$input" -i "$watermark" \
-			-filter_complex "$blurstr [1][0]scale2ref[i][m];[m][i]overlay=format=auto[v];
-			$memestr $rotatestr format=yuv420p [v]" \
-			-map "[v]" -f v4l2 "/dev/video$output"
-	else
-		echo "here wowow"
-		ffmpeg -i "/dev/video$input" $inputstr \
-			-filter_complex "[0]split=1[v]; $blurstr $memestr $rotatestr
-			[v]format=yuv420p[v]" \
-			-map "[v]" -f v4l2 "/dev/video$output"
-
-	fi
+if [[ $printffmpeg != false ]]
+then
+	echo "Generated command:"
+	echo "$ffmpegstr"
 fi
-# ffmpeg -i $norm -c copy v1.mp4
-# ffmpeg -i $rev -c copy v2.mp4
 
-# ffmpeg -i v1.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts input1.ts
-# ffmpeg -i v2.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts input2.ts
-
-# ffmpeg -i "concat:input1.ts|input2.ts" -c copy "$now.mp4"
-
-# ffmpeg -f concat -safe 0 -i mylist.txt -c copy "$now 2.mp4"
-
-# rm v1.mp4 v2.mp4 input1.ts input2.ts $rev
-
-# ffmpeg -stream_loop -1 -re -i "$video.mp4" -map 0:v -f v4l2 /dev/video2
-
-# sh rotate_video.sh "$now.mp4"
-
-# ffmpeg -f v4l2 -input_format yuyv422 -video_size 800x448 -framerate 30 -i /dev/video0 -filter_complex "[0]vflip,drawtext=fontfile=/usr/share/fonts/truetype/freefont/Roboto-Regular.ttf:text='%{localtime\:%d %b %Y}':x=8:y=8:fontcolor=white:box=1:boxcolor=black@0.75,drawtext=fontfile=/usr/share/fonts/truetype/freefont/Roboto-Regular.ttf:text='%{localtime\:%T}':x=8:y=24:fontcolor=white:box=1:boxcolor=black@0.75[vid];[0]fps=1/20,vflip[img]" -map "[vid]" -c:v h264_omx -b:v 2M -f rtsp rtsp://localhost:8554/mystream -map "[img]" -update 1 /dev/shm/snapshot.jpg
+sh -c "$ffmpegstr"
