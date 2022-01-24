d55a0af175a793b843c71d9fe87e27dcdfea0e8b
Andrew Mascillaro <amascillaro@gmail.com>
2022-01-24 16:39:06 -0500

    Switch from pure shell to bash directive Also print version number upon manual installation

--- install.sh
+++ install.sh
@@ -4,9 +4,11 @@ install -Dm644 ./help.md "/usr/local/share/weffe/"
 
 install -t "/usr/local/share/weffe/static" static/*.png
 
-echo "#!/bin/sh
+echo "#!/bin/bash
  /usr/local/share/weffe/weffe \"\$@\"" >> ./tempbin
 install -Dm755 ./tempbin "/usr/local/bin/weffe"
 rm ./tempbin
 
-echo "Successfully installed weffe locally"
+version=$(git describe --long | sed 's/^v//;s/\([^-]*-g\)/r\1/;s/-/./g')
+
+echo "Successfully installed weffe v$version locally"
